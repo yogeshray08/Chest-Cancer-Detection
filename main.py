3d043b2fba1c8138bfb5830f5438b785522c823e
Yogesh Kumar <rayyogesh08032002@gmail.com>
2026-06-02 04:44:13 +0530

    stage1

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-# from cnnClassifier import logger
-# from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 # from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 # from cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 # from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 
 
-# STAGE_NAME = "Data Ingestion stage"
+STAGE_NAME = "Data Ingestion stage"
 
 
-# try:
-#     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#     obj = DataIngestionTrainingPipeline()
-#     obj.main()
-#     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 
 
@@ -60,6 +60,3 @@
 # except Exception as e:
 #         logger.exception(e)
 #         raise e
-
-from cnnClassifier import logger
-logger.info("Training pipeline started")
