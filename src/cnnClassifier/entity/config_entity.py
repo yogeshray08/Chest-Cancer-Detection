ba7d73080dbe2880063507819f033c529731cd47
Yogesh Kumar <rayyogesh08032002@gmail.com>
2026-06-02 08:36:51 +0530

    stage 3

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -25,16 +25,16 @@ class PrepareBaseModelConfig:
 
 
 
-# @dataclass(frozen=True)
-# class TrainingConfig:
-#     root_dir: Path
-#     trained_model_path: Path
-#     updated_base_model_path: Path
-#     training_data: Path
-#     params_epochs: int
-#     params_batch_size: int
-#     params_is_augmentation: bool
-#     params_image_size: list
+@dataclass(frozen=True)
+class TrainingConfig:
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augmentation: bool
+    params_image_size: list
 
 
 
